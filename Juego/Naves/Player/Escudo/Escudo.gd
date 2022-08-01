d0e6817dae8ceb92e888935bd5e19bfa44519bbc
M1gu3lC <miguelangelcf199520@gmail.com>
2022-08-01 18:20:59 -0300

    semana 3 clase 4 Estacion de recarga

--- Juego/Naves/Player/Escudo/Escudo.gd
+++ Juego/Naves/Player/Escudo/Escudo.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 ## Variables
 var esta_activado:bool = false setget ,get_esta_activado
+var energia_original:float
 
 ## Variables Export
 export var energia:float = 8.0
@@ -11,16 +12,15 @@ export var radio_desgaste:float = -1.6
 ## Setters y Getters
 func get_esta_activado() -> bool:
 	return esta_activado
+	
 ## Metodos
 func _ready() -> void:
+	energia_original = energia
 	set_process(false)
 	controlar_colisionador(true)
 
 func _process(delta: float) -> void:
-	energia += radio_desgaste * delta
-	
-	if energia <= 0.0:
-		desactivar()
+	controlar_energia(radio_desgaste * delta)
 
 func desactivar() -> void:
 	set_process(false)
@@ -29,6 +29,14 @@ func desactivar() -> void:
 	$AnimationPlayer.play_backwards("activando")
 
 ## Metodos Custom
+func controlar_energia(consumo: float) -> void:
+	energia += consumo
+	
+	if energia > energia_original:
+		energia = energia_original
+	elif energia <= 0.0:
+		desactivar()
+		
 func controlar_colisionador(esta_desactivado: bool) -> void:
 	$CollisionShape2D.set_deferred("disabled", esta_desactivado)
 
